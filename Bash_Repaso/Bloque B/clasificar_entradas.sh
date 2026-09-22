cbca5677362a4c7ec23d4c24f5327f3c131b50b6
Bogdan Lambru <boglam@alu.edu.gva.es>
2026-09-22 12:28:47 +0200

    Ejercicios Bloque B y C, corregidos

--- Bash_Repaso/Bloque B/clasificar_entradas.sh
+++ Bash_Repaso/Bloque B/clasificar_entradas.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+for entrada in /home/boglam/prueba_bash/*; do
+    nombre=$(basename "$entrada")
+    if [[ -d $entrada ]]; then
+        echo "$nombre: directorio"
+    elif [[ -f $entrada]]; then
+        echo "$nombre: fichero"
+    fi
+done 
